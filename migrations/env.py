6ae4b4b73060fbb2152948315e2578dbc6b80635
Logan Fate <logan.m.fate@gmail.com>
2024-03-01 17:21:21 -0500

    fixed migration script errors

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,10 @@ from logging.config import fileConfig
 from flask import current_app
 from alembic import context
 
-SCHEMA_NAME = os.getenv('SCHEMA')
+FLASK_ENV = os.getenv('FLASK_ENV', 'development')
+
+
+SCHEMA_NAME = os.getenv('SCHEMA') if FLASK_ENV == 'production' else None
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
