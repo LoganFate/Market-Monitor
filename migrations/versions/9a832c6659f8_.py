30eb85f6acb2bd98d8e92e06504a386aae372546
Logan Fate <logan.m.fate@gmail.com>
2024-03-28 10:57:57 -0500

    edited article in comments table to integer

--- migrations/versions/9a832c6659f8_.py
+++ migrations/versions/9a832c6659f8_.py
@@ -68,7 +68,7 @@ def upgrade():
     op.create_table('comments',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('user_id', sa.Integer(), nullable=False),
-    sa.Column('article_id', sa.String(), nullable=False),
+    sa.Column('article_id', sa.Integer(), nullable=False),
     sa.Column('text', sa.Text(), nullable=False),
     sa.Column('created_at', sa.DateTime(), nullable=False),
     sa.ForeignKeyConstraint(['article_id'], [f'{SCHEMA}.articles.id'] if environment == 'production' else ['articles.id'], ondelete='CASCADE'),
